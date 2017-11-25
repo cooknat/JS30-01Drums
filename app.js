dadce71e31bc58fae0f1c53ba524a90abeddca5a
Natalie Cook <natalie.t.cook@gmail.com>
2017-11-25 08:56:08 +0000

    done

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
-window.addEventListener('keydown', playSound);
+document.addEventListener('DOMContentLoaded', () => {
 
-function playSound(e){
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  window.addEventListener('keydown', playSound);
 
-  if(!audio) return;
+  function playSound(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
-  audio.currentTime = 0; //rewind
-  audio.play();
-  key.classList.add("playing");
-};
+    if(!audio) return;
 
-var removeTransition = function(e){
-  console.log(e);
-  if(e.propertyName !== "transform") return;
-  this.classList.remove('playing');
-};
+    audio.currentTime = 0; //rewind
+    audio.play();
+    key.classList.add("playing");
+  };
 
+  var removeTransition = function(e){
+    if(e.propertyName !== "transform") return;
+    this.classList.remove('playing');
+  };
 
-const keys = document.querySelectorAll(".key");
-console.log(keys);
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  const keys = document.querySelectorAll(".key");
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+});
